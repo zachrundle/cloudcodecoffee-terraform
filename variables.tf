260a260d1ae81a7c8fe3d24994d37e6f18f847c0
cybershady <zjrundle@gmail.com>
2023-12-28 16:42:36 -0700

    finalize network module changes, adding iam module

--- variables.tf
+++ variables.tf
@@ -11,3 +11,4 @@ variable "region" {
 variable "aws_account" {
   description = "Account number to create AWS resources in. This variable should be defined in the Terraform Cloud workspace settings"
 }
+
