60a5f40401894e014274153af916c976b29c1f3f
cybershady <zjrundle@gmail.com>
2023-12-29 11:38:00 -0700

    fixed typo in iam module attempt2

--- modules/iam/users/main.tf
+++ modules/iam/users/main.tf
@@ -7,13 +7,17 @@ resource "aws_iam_user" "this" {
 }
 
 resource "aws_iam_user_login_profile" "this" {
-  for_each                = { for user in var.users_and_groups : user.iam_username => user }
+  for_each = {
+    for user in var.users_and_groups : user.is_service_account ? "svc-${user.iam_username}" : user.iam_username => user
+  }
   user                    = aws_iam_user.this[each.key].name
   password_reset_required = var.password_reset_required
 }
 
 resource "aws_iam_user_group_membership" "iam_user_group_memberships" {
-  for_each = { for user in var.users_and_groups : user.is_service_account ? "svc-${user.iam_username}" : user.iam_username => user }
+  for_each = {
+    for user in var.users_and_groups : user.is_service_account ? "svc-${user.iam_username}" : user.iam_username => user
+  }
   user     = aws_iam_user.this[each.key].name
   groups   = each.value.iam_groups
 }
