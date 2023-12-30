c0ccf7101030fcc6360e7cda940b0c4d16bb3fb6
cybershady <zjrundle@gmail.com>
2023-12-30 00:12:04 -0700

    add policy attachment

--- modules/iam/groups/main.tf
+++ modules/iam/groups/main.tf
@@ -3,3 +3,9 @@ resource "aws_iam_group" "this" {
   name     = each.key
 }
 
+resource "aws_iam_group_policy_attachment" "this" {
+  for_each = { for group, details in var.iam_groups : group => flatten([for policy in details.policies : { group = group, policy_arn = policy }]) }
+
+  group      = aws_iam_group.this[each.value[0].group].name
+  policy_arn = each.value[0].policy_arn
+}
