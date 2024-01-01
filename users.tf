8fb7155abad114295ff99c5afa244be580667d22
cybershady <zjrundle@gmail.com>
2023-12-31 19:51:26 -0700

    remove unneeded comments

--- users.tf
+++ users.tf
@@ -1,10 +1,7 @@
-# Separting out the creation if IAM users because of how quickly this will grow
+# Separating out the creation of IAM users because of how quickly this will grow
 module "iam_users" {
   source = "./modules/iam/users"
 
-  # TODO: Even though defaults are set for the variable users_and_groups, it still requires all values explicitely defined
-  # if is_service_account = true, it will prefix the iam username with "svc_" to easily track resources
-  # TODO: explore using tags for account_type: user/service
   # The process of setting the initial user password will be done by the sysadmin
   # The process of generating API keys should be done by the user once default password has been changed
 
