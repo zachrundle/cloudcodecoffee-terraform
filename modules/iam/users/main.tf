0933a9d986d2c2b1c0c5368c99635f07660a45dc
cybershady <zjrundle@gmail.com>
2024-01-23 20:15:58 -0700

    update iam user module

--- modules/iam/users/main.tf
+++ modules/iam/users/main.tf
@@ -1,4 +1,12 @@
 # TODO: find a better way for password management, as this is not a realistic solution
+resource "aws_iam_account_password_policy" "this" {
+  minimum_password_length = 12
+  require_lowercase_characters = true
+  require_uppercase_characters = true
+  require_numbers = true
+  require_symbols = true
+}
+
 
 resource "aws_iam_user" "this" {
   for_each = { for user in var.users_and_groups : user.iam_username => user }
@@ -8,14 +16,20 @@ resource "aws_iam_user" "this" {
 }
 
 resource "aws_iam_user_login_profile" "this" {
-  for_each = { for user in var.users_and_groups : user.iam_username => user }
+  for_each = { for user in var.users_and_groups : user.iam_username => user if !user.is_service_account}
 
   user                    = aws_iam_user.this[each.key].name
   password_reset_required = var.password_reset_required
   password_length         = 20
 }
 
-resource "aws_iam_user_group_membership" "iam_user_group_memberships" {
+resource "aws_iam_access_key" "this" {
+  for_each = { for user in var.users_and_groups : user.iam_username => user }
+  user       = each.key
+  depends_on = [aws_iam_user.this]
+}
+
+resource "aws_iam_user_group_membership" "this" {
   for_each = { for user in var.users_and_groups : user.iam_username => user }
 
   user   = aws_iam_user.this[each.key].name
